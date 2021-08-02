9a9aa1484dd6683a066b09bb38af0d104639b367
itlognamaalat21 <rivennation201@gmail.com>
2021-08-02 19:31:12 +0800

    push also

--- biography.php
+++ biography.php
@@ -6,8 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biography</title>
 </head>
+<style>
+    .header{
+        font-family: arial;
+    }
+    </style>
 <body>
+    <div class="header">
+        <h1>EZEKIEL OJERIO'S BIOGRAPHY</h1>
     <?php
+    echo "Name: Ezekiel C. Ojerio<br>";
+    echo "Birthday: November 23, 2001<br>";
+    echo "Age: 19<br>";
+    echo "Birthplace: Makati City<br>";
+    echo "Hobbies: Computer Games, Reading, Chess and Puzzle games";
     ?>
+    </div>
 </body>
 </html>
